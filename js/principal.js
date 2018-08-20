963c1426300bea9f86e6ca7c68139f6ba0926634
Filipe de Andrade Morais <afilipem@gmail.com>
2018-08-19 21:42:50 -0300

    changes and new: moving the js code from the html to principal.js and changing the queryselector to look for a class

---
+++ js/principal.js
@@ -0,0 +1,7 @@
+        alert("Olá mundo");
+		console.log("Oi Mundo");
+		console.log(document);
+		console.log(document.querySelector("h1"));
+		var titulo = document.querySelector(".titulo");
+		console.log(titulo.textContent);
+		titulo.textContent = "Banana";  
