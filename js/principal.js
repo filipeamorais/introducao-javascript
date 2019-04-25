e9835fbcab7002525f327b359c7f56c3da15961c
Filipe de Andrade Morais <afilipem@gmail.com>
2019-04-25 18:25:55 -0500

    new: adding the file of the comments of the course and previous edtings that were not commited

--- js/principal.js
+++ js/principal.js
@@ -1,24 +1,24 @@
-        alert("Olá mundo");
-		console.log("Oi Mundo");
-		console.log(document);
-		console.log(document.querySelector("h1"));
-		var titulo = document.querySelector(".titulo");
-		console.log(titulo.textContent);
-		titulo.textContent = "Aparecida Nutricionista";
+    //     alert("Olá mundo");
+		// console.log("Oi Mundo");
+		// console.log(document);
+		// console.log(document.querySelector("h1"));
+		// var titulo = document.querySelector(".titulo");
+		// console.log(titulo.textContent);
+		// titulo.textContent = "Aparecida Nutricionista";
 
     var pacientes = document.querySelectorAll(".paciente");
+    var paciente = pacientes[i];
 
-    for (var i=0; i<pacientes.lenght;i++){
+    for (var i=0; i<pacientes.length;i++){
       var paciente = pacientes[i];
+
       var tdPeso = paciente.querySelector(".info-peso");
       var tdAltura = paciente.querySelector(".info-altura");
 
+      var tdImc = paciente.querySelector(".info-imc");
+
       var peso = tdPeso.textContent;
       var altura = tdAltura.textContent;
-
-      var imc = peso / (altura*altura);
-      var tdImc = paciente.querySelector(".info-imc");
-      tdImc.textContent = imc;
 
       var alturaEhValida = true;
       var pesoEhValido = true;
@@ -27,12 +27,14 @@
       console.log("Peso inválido");
       tdImc.textContent = "Peso inválido!";
       pesoEhValido = false;
+      paciente.classList.add("paciente-invalido");
       }
 
       if(altura <= 0 || altura >= 3){
       console.log("Altura inválida");
       tdImc.textContent = "Altura inválida!";
       alturaEhValida = false;
+      paciente.classList.add("paciente-invalido");
       }
 
       if(pesoEhValido && alturaEhValida){
