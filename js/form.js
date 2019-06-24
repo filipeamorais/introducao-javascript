56a12550b7176a3bca053baa8cd961015657f5c8
filipeamorais@pm.me <filipeamorais@pm.me>
2019-06-24 11:56:46 -0500

    new: commented the different small uses of the code form.js and started to create a function for extracting info from the form.

--- js/form.js
+++ js/form.js
@@ -2,13 +2,15 @@ botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
     var form = document.querySelector("#form-adiciona");
 
+    obtemPacienteDoFormulario(form)
+    //Extraindo informacoes do paciente do form
     var nome = form.nome.value;
     var peso = form.peso.value;
     var altura = form.altura.value;
     var gordura = form.gordura.value;
 
+    //cria a tr e a td do paciente
     var pacienteTr = document.createElement("tr");
-
     var nomeTd = document.createElement("td");
     var pesoTd = document.createElement("td");
     var alturaTd = document.createElement("td");
@@ -21,6 +23,7 @@ botaoAdicionar.addEventListener("click", function(event){
     gorduraTd.textContent = gordura;
     imcTd.textContent = calculaImc(peso, altura);
 
+    //adiciona o paciente na tabela
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
@@ -34,3 +37,7 @@ botaoAdicionar.addEventListener("click", function(event){
 
     console.log("Oi eu sou o botão e fui clicado")
   });
+
+  function obtemPacienteDoFormulario (form) {
+    
+  }
