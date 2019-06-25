2462a3e39fd0385ec7cd56a067e7fbfc0c6088f6
filipeamorais@pm.me <filipeamorais@pm.me>
2019-06-24 21:11:42 -0500

    new: created a new function to insert a new tr with data.

--- js/form.js
+++ js/form.js
@@ -5,27 +5,9 @@ botaoAdicionar.addEventListener("click", function(event){
     //Extraindo informacoes do paciente do form
     var paciente = obtemPacienteDoFormulario(form);
     console.log(paciente);
-    
+
     //cria a tr e a td do paciente
-    var pacienteTr = document.createElement("tr");
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso, altura);
-
-    //adiciona o paciente na tabela
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    var pacienteTr = montaTr(paciente);
 
     var tabela = document.querySelector("#tabela-pacientes");
 
@@ -44,4 +26,27 @@ botaoAdicionar.addEventListener("click", function(event){
       imc: calculaImc(form.peso.value, form.altura.value)
     }
     return paciente;
+  }
+
+  function montaTr(paciente){
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = paciente.nome;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.textContent = paciente.imc;
+
+    //adiciona o paciente na tabela
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+    return pacienteTr;
   }
